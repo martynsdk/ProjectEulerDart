67254db3ce469945f5459854e9a0768a1f798e9d
martynfigueiredo <martyn.figueiredo@pearson.com>
2022-03-22 07:15:29 -0600

    Even Fibonacci Numbers

--- lib/Exercises/01_multiples_of_3_or_5.dart
+++ lib/Exercises/01_multiples_of_3_or_5.dart
@@ -1,7 +1,9 @@
-/***
- * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
- * Find the sum of all the multiples of 3 or 5 below 1000.
- */
+
+import 'package:flutter/foundation.dart';
+
+/// *
+/// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+/// Find the sum of all the multiples of 3 or 5 below 1000.
 
 main() {
   int number = 1000;
@@ -11,5 +13,7 @@ main() {
       sum += i;
     }
   }
-  print ("The sum of all the multiples of 3 or 5 below 1000 is $sum");
+  if (kDebugMode) {
+    print ("The sum of all the multiples of 3 or 5 below 1000 is $sum");
+  }
 }
