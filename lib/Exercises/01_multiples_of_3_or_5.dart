5360dec0a203154f179157e73b7beb00f7a2c9e9
martynsdk <martynsdk@gmail.com>
2022-03-23 10:39:06 -0600

    /// Results will be printed on Console.

--- lib/Exercises/01_multiples_of_3_or_5.dart
+++ lib/Exercises/01_multiples_of_3_or_5.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 /// *
 /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 /// Find the sum of all the multiples of 3 or 5 below 1000.
+/// Results will be printed on Console.
 
 main() {
   int number = 1000;
