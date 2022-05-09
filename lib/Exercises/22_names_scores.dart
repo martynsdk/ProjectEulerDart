ef69f053927064a2aa713db6033e680a46576b92
martynsdk <martynsdk@gmail.com>
2022-05-09 11:10:28 -0300

    Names Scores

--- lib/Exercises/22_names_scores.dart
+++ lib/Exercises/22_names_scores.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-/// Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order.
+/// Using p022_names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order.
 /// Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
 /// For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
 /// So, COLIN would obtain a score of 938 × 53 = 49714.
@@ -8,7 +8,7 @@ import 'dart:io';
 /// Results will be generated on the Console
 
 main() {
-  List<String> names = File('lib/Exercises/aux/names.txt')
+  List<String> names = File('lib/Exercises/aux/p022_names.txt')
       .readAsStringSync()
       .replaceAll('"', '')
       .split(',');
